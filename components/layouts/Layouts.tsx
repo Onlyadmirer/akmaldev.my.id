422b53b2c6a703d2fa26a541f04b8ae9fccf109b
Onlyadmirer <akmalrbc6@gmail.com>
2025-11-08 19:37:35 +0800

    feat: add background

--- components/layouts/Layouts.tsx
+++ components/layouts/Layouts.tsx
@@ -1,10 +1,14 @@
 import Navbar from "../shared/navbar/Navbar";
+import Background from "./background/Background";
 
 function Layouts({ children }: { children: React.ReactNode }) {
   return (
-    <div className='max-w-6xl mx-auto px-4 py-6 grid grid-cols-[250px_1fr] min-h-screen'>
-      <Navbar />
-      <main>{children}</main>
+    <div className='max-w-6xl mx-auto px-4 relative py-6  min-h-screen'>
+      <Background />
+      <div className='grid grid-cols-[250px_1fr]'>
+        <Navbar />
+        <main>{children}</main>
+      </div>
     </div>
   );
 }
