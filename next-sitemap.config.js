55340ffd46a5db060059d3e6dbdc238e2e0c9844
Onlyadmirer <akmalrbc6@gmail.com>
2025-11-15 08:52:13 +0800

    feat: add sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.akmaldev.my.id',
+
+  generateRobotsTxt: true,
+
+  outDir: './public',
+
+};
