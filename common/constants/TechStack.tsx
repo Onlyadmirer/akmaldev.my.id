5889c451cd6eb58a6a9add6dd77b05ced9a0f7c9
Onlyadmirer <akmalrbc6@gmail.com>
2025-11-22 10:38:30 +0800

    feat: update card projects

--- common/constants/TechStack.tsx
+++ common/constants/TechStack.tsx
@@ -1,5 +1,20 @@
-export const STACK = {
+type StackProps = {
+  [key: string]: {
+    icon: string;
+    link: string;
+    key: string;
+  };
+};
+
+export const STACK: StackProps = {
   TailwindCSS: {
     icon: "/skillsIcon/tailwindcss.svg",
+    link: "https://tailwindcss.com/docs/installation/using-vite",
+    key: "tailwindcss",
+  },
+  Nextjs: {
+    icon: "/skillsIcon/nextjs.svg",
+    link: "https://nextjs.org",
+    key: "nextjs",
   },
 };
--- lib/data.ts
+++ lib/data.ts
@@ -5,13 +5,20 @@ export const getInfo = async () => {
     where: { email: "akmalrbc6@gmail.com" },
     include: {
       achievements: true,
-      projects: true
+      projects: {
+        include: {
+          stack: true
+        }
+      },
     }
   })
 
   const userAchievements = user?.achievements || []
 
-  const userProjects = user?.projects || []
+  const userProjects = (user?.projects || []).map((project) => ({
+    ...project,
+    stack: project.stack.map((s) => s.name)
+  }))
 
   if (!user) {
     throw new Error('User not found');  // Atau return empty + log
